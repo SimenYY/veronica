deab749a87b594b69a4ba519ee86874627f1d644
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-06-06 10:56:30 +0800

    feat(base): 新增 DataModel 类
    - 添加 DataModel 类，提供将 dataclass 转换为字典的方法
    - 实现 to_dict 方法，支持排除值为 None 的字段
    - 为项目提供统一的数据模型基类

---
+++ veronica/base/models.py
@@ -0,0 +1,29 @@
+
+
+
+class DataModel:
+    def to_dict(
+        self, 
+        *, 
+        exclude_none: bool = False,
+        
+    ) -> dict:
+        """ Convert dataclass to dict
+
+        Notes:
+            * A TypeError will be raised if self contains a field that cannot be deep copied (e.g. sys.stdout).
+        
+        
+        :param bool exclude_none: _description_, defaults to False
+        :raises TypeError: _description_
+        :return dict: _description_
+        """
+        from dataclasses import asdict, is_dataclass
+        
+        if not is_dataclass(self):
+            raise TypeError("to_dict() should be called on dataclass instances")
+        
+        if exclude_none:
+            return {k: v for k, v in asdict(self).items() if v is not None}
+        
+        return asdict(self)
