4beff4f8d5b5c73d03a01374ee40b2efabc74c4b
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-06-11 10:04:59 +0800

    feat(log): 重构 LoguruConfig 类并添加新功能
    - 将 LoguruConfig 改为 dataclass
    - 添加 load 方法，支持从字典加载配置
    - 添加 configure 方法，支持应用配置
    - 添加 clear 方法，用于清除现有配置
    - 修改 add_handler 方法，使其返回 handler ID
    - 移除 get_logger 方法

--- veronica/core/log/loguru_config.py
+++ veronica/core/log/loguru_config.py
@@ -1,9 +1,17 @@
 import logging
+from dataclasses import dataclass, field
 from typing import (
     Type,
+    Dict, 
+    Any,
+    List,
+    Optional,
+    Self,
     TYPE_CHECKING
 )
+from pathlib import Path
 import loguru
+
 
 if TYPE_CHECKING:
     pass
@@ -11,23 +19,60 @@ if TYPE_CHECKING:
 from veronica.core.log.logging_handlers import InterceptToLoguruHandler
 from veronica.core.log.loguru_handlers import BaseHandler
 
-
 __all__ = [
     "LoguruConfig"
 ]
 
-
-class LoguruConfig:
+@dataclass
+class LoguruConfig():
     """loguru configuration
     """
-    loguru.logger.remove()
+    handlers: List[BaseHandler] = field(default_factory=list)
     
-    @staticmethod
-    def add_handler(handler: Type[BaseHandler]) -> None:
-        loguru.logger.add(**handler().to_dict())
-            
-    @staticmethod
+    @classmethod
+    def load(
+        cls, 
+        source: Dict[str, Any], 
+        is_configured: bool = True
+    ) -> Optional[Self]:
+        """load  a config from a dict
+        
+        Usage:
+            config = {
+                "handlers": [
+                    BaseHandler1(),
+                    BaseHandler2().
+                ]
+            }        
+
+        :param dict source: _description_
+        :param bool is_configured: _description_, defaults to True
+        :return Optional[Self]: _description_
+        """
+        config = cls(**source)
+        if is_configured:
+            config.configure()
+            return None
+        
+        return config
+    def configure(self) -> List[int]:
+        config: Dict[str, Any] = {
+            "handlers": [handler.to_dict() for handler in self.handlers ]
+        }
+        return loguru.logger.configure(**config)
+    
+    @classmethod
+    def clear(cls) -> None:
+        loguru.logger.remove()
+
+    def add_handler(self, handler: BaseHandler) -> int:        
+        self.handlers.append(handler)
+        
+        return loguru.logger.add(**handler.to_dict())
+
+    @classmethod
     def intercept_logging(
+        cls,
         namespace: str, 
         level: str = "DEBUG"
     ) -> None:
@@ -41,7 +86,7 @@ class LoguruConfig:
         logging_logger.setLevel(level)
         logging_logger.handlers.clear()
         logging_logger.addHandler(InterceptToLoguruHandler())
-        
-    @staticmethod
-    def get_logger():
-        return loguru.logger
+    
+    
+    
+    
