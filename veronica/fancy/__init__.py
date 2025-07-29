7f2f80c613b2578e1e4d50aa4b4da41b86a96faa
AdzLovelace <heyinyu.sensi@foxmail.com>
2025-07-29 10:50:45 +0800

    fix(veronica.fancy): 增加 art 模块的导入异常处理
    - 在 __init__.py 文件中添加了 try-except 块
    - 当 art 模块未安装时，抛出 ImportError 异常
    - 提示用户使用 pip install art 进行安装

--- veronica/fancy/__init__.py
+++ veronica/fancy/__init__.py
@@ -1,2 +1,6 @@
-from art import text2art as text2art
-from art import tprint as tprint
+try:
+
+    from art import text2art as text2art
+    from art import tprint as tprint
+except ImportError:
+    raise ImportError("art is not installed., Please install it using pip insall art")
